eb6688c7cc9a3582c440c295ba4856655b3ef32b
Adi-Marghade <aditya.marghade.batch2025@sitnagpur.siu.edu.in>
2026-02-10 15:29:08 +0530

    Create Ex 5.1.2.py

---
+++ Experiment 5/Ex 5.1.2.py
@@ -0,0 +1,15 @@
+a,b,c,d = map(int,input().split())
+total = a+b+c+d
+percent = float((total/400)*100)
+print(total)
+print(f"{percent:.2f}")
+if percent > 75:
+	print("Distinction")
+elif percent >= 60 and percent < 75:
+	print("First Division")
+elif percent >=50 and percent < 60:
+	print("Second Division")
+elif percent >= 40 and percent< 60:
+	print("Third Division")
+else:
+	print("Fail")
